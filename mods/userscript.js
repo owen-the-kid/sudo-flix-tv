9ae201349e27bb258c18c91df93fadcaf70807e9
CryoCode <owenramaekers2006@gmail.com>
2024-06-07 11:51:39 +0200

    modified:   mods/userscript.js

---
+++ mods/userscript.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    const script = document.createElement('script');
+    script.src = 'https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js';
+    script.onload = function() {
+        initSpatialNavigation();
+    };
+    document.head.appendChild(script);
+
+    function initSpatialNavigation() {
+        const focusableElements = document.querySelectorAll('a, .focusable');
+
+        focusableElements.forEach(element => {
+            element.addEventListener('focus', () => {
+                element.style.outline = '2px solid red';
+            });
+
+            element.addEventListener('blur', () => {
+                element.style.outline = '';
+            });
+        });
+
+        window.SpatialNavigation.init();
+        focusableElements.forEach(element => {
+            window.SpatialNavigation.add(element);
+        });
+
+        if (focusableElements.length > 0) {
+            focusableElements[0].focus();
+        }
+    }
+})();
